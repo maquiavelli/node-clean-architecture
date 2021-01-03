cd30ea74cf09275a26e1cfd614b99539dc8aaff0
Gabriel Maquiavelli <gabrielmaquiavelli@gmail.com>
2021-01-03 20:16:59 -0300

    simplify code

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -16,15 +16,13 @@ module.exports = class AuthUseCase {
     }
 
     const user = await this.loadUserByEmailRepository.load(email);
-    if (!user) {
-      return null;
-    }
-    const isValid = await this.encrypter.compare(password, user.password);
-    if (!isValid) {
-      return null;
-    }
+    const isValid =
+      user && (await this.encrypter.compare(password, user.password));
 
-    const accessToken = await this.tokenGenerator.generate(user.id);
-    return accessToken;
+    if (isValid) {
+      const accessToken = await this.tokenGenerator.generate(user.id);
+      return accessToken;
+    }
+    return null;
   }
 };
