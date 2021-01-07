e431734d99ce5467eb22e3d4f0955750aa5786a3
Gabriel Maquiavelli <gabrielmaquiavelli@gmail.com>
2021-01-06 21:57:09 -0300

    move TokenGenerator to its own file

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,22 +1,6 @@
 const jwt = require('jsonwebtoken');
 const MissingParamError = require('../errors/missing-param-error');
-
-class TokenGenerator {
-  constructor(secret) {
-    this.secret = secret;
-  }
-
-  async generate(id) {
-    if (!this.secret) {
-      throw new MissingParamError('secret');
-    }
-    if (!id) {
-      throw new MissingParamError('id');
-    }
-    this.id = id;
-    return jwt.sign(this.id, this.secret);
-  }
-}
+const TokenGenerator = require('./token-generator');
 
 const makeSut = () => new TokenGenerator('secret');
 
