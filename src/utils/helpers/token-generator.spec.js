ef283a14f7aee2cd200be1c274656fb293660476
Gabriel Maquiavelli <gabrielmaquiavelli@gmail.com>
2021-01-06 21:53:40 -0300

    ensure TokeGenerator throws if no id is provided

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -10,6 +10,9 @@ class TokenGenerator {
     if (!this.secret) {
       throw new MissingParamError('secret');
     }
+    if (!id) {
+      throw new MissingParamError('id');
+    }
     this.id = id;
     return jwt.sign(this.id, this.secret);
   }
@@ -42,5 +45,11 @@ describe('Token Generator', () => {
     const sut = new TokenGenerator();
     const promise = sut.generate('any_id');
     expect(promise).rejects.toThrow(new MissingParamError('secret'));
+  });
+
+  test('Should throw if no id is provided', async () => {
+    const sut = makeSut();
+    const promise = sut.generate();
+    expect(promise).rejects.toThrow(new MissingParamError('id'));
   });
 });
