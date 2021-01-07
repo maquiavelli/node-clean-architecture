63351d373f30f8b8de926c4922a8cea84c9e4ee2
Gabriel Maquiavelli <gabrielmaquiavelli@gmail.com>
2021-01-06 21:36:05 -0300

    ensure TokenGenerator returns a token if JWT returns token

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,14 +1,22 @@
+const jwt = require('jsonwebtoken');
+
 class TokenGenerator {
   async generate(id) {
     this.id = id;
-    return null;
+    return jwt.sign(this.id, 'secret');
   }
 }
 
 describe('Token Generator', () => {
   test('Shoul return null if JWT returns null', async () => {
     const sut = new TokenGenerator();
+    jwt.token = null;
     const token = await sut.generate('any_id');
     expect(token).toBe(null);
+  });
+  test('Shoul return a token if JWT returns token', async () => {
+    const sut = new TokenGenerator();
+    const token = await sut.generate('any_id');
+    expect(token).toBe(jwt.token);
   });
 });
